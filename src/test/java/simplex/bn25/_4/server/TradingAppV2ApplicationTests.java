d3fc7856464ccd1689baf70f3f4c9cfe7212792f
nakagawaryogo <nakaryou1214@gamil.com>
2025-04-17 23:07:17 +0900

    Initial commit

---
+++ src/test/java/simplex/bn25/_4/server/TradingAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package simplex.bn25._4.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
